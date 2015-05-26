ea0842942119e2711e489ae390f5d48c73254ee3
pvincent <paul.vincent.ecole@gmail.com>
2015-05-26 16:32:59 +0200

    Getset now go in src/ and inc/ files

--- getset.sh
+++ getset.sh
@@ -1,35 +1,36 @@
 class=$1
 type=$2
 name=$3
-class_filec=$1
-class_fileh=$1
-class_filec+=".class.cpp"
-class_fileh+=".class.hpp"
-if [ $1 ] && [ $2 ] && [ $3 ] && [ -s "$class_filec" ] && [ -s "$class_fileh" ] ;then
-echo "" >> $class.class.cpp
-echo $2 >> $class.class.cpp
+class_filec="src/$1"
+class_fileh="inc/$1"
+class_filec+=".cpp"
+class_fileh+=".hpp"
+echo $class_file
+echo "" >> src/$class.cpp
+echo $2 >> src/$class.cpp
 echo "$class::get$name(void) const
 {
 	return (this->_$name);
-}" >> $class.class.cpp
+}" >> src/$class.cpp
 
-echo "" >> $class.class.cpp
-echo "void" >> $class.class.cpp
+echo "" >> src/$class.cpp
+echo "void" >> src/$class.cpp
 echo "$class::set$name($type const $name)
 {
 	this->_$name = $name;
 	return ;
-}" >> $class.class.cpp
-
+}" >> src/$class.cpp
 sed -i '.tmp' $'/private:/ i\
 \\\t\t'"$2 "'get'"$3"'(void)'" const;
-" $1.class.hpp
+" inc/$1.hpp
 sed -i '.tmp' $'/private:/ i\
 \\\t\t'"void "'set'"$3($2 const $3);\\
 \\
-"  $1.class.hpp
+"  inc/$1.hpp
+sed -i '.tmp' $'/private:/ a\
+\\\t\t'"$2"$'\\\t'"_$3;\\
+\\
+"  inc/$1.hpp
 
-rm -rf $1.class.hpp.tmp
-else
-	echo "I need a ClassName, a Type and a Name.. or maybe, $class_filec does'nt exist"
-fi
+
+rm -rf $1.hpp.tmp
